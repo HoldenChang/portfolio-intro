5fc61e74bee78715402d0fc6db47eeb467895947
Holden Chang <ncsm1752@naver.com>
2024-01-09 18:43:45 -0800

    worked on intro
    worked on intro background

--- JS/jsEffect.js
+++ JS/jsEffect.js
@@ -36,9 +36,14 @@ window.addEventListener("scroll", function () {
 });
 
 function setDivHeight() {
-    const windowHeight = window.innerHeight-100;
+
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    const vwToPixels = (7 * viewportWidth) / 100;
+    const windowHeight = window.innerHeight - vwToPixels;
     const introDiv = document.querySelector('.intro');
+    
     introDiv.style.height = `${windowHeight}px`;
+
 }
 
 setDivHeight();
