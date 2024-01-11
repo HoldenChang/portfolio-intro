97d2ccc7298c9132e5035dd677bed0bb59cbbf61
Holden Chang <ncsm1752@naver.com>
2024-01-11 13:12:04 -0800

    Project Section Modification1

--- JS/jqEffect.js
+++ JS/jqEffect.js
@@ -5,58 +5,27 @@ $(document).ready(()=>{
         $(".menu").toggleClass("show");
 
         var currentExpandedState = $('#headerToggle').attr("aria-expanded");
+
         $('#headerToggle').attr("aria-expanded", currentExpandedState === "false" ? "true" : "false");
     });
 
-    $('.project1').on('mouseenter', (event)=>{
-        $('.project1').css({
-            'transform': 'scale(0.8)',
-            'border': '1px solid red',
-            'transition': '.5s'
-        });
-        $('.project1 img').css({
-            'opacity': '0.7'
-        });
-        $('.project1 p').css({
-            'opacity': '1'
-        });
-    }).on('mouseleave', (event)=>{
-        $('.project1').css({
-            'transform': 'scale(1)',
-            'border': 'none',
-            'transition': '.5s'
-        });
-        $('.project1 img').css({
-            'opacity': '1'
-        });
-        $('.project1 p').css({
-            'opacity': '0'
-        });
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horSection = gsap.utils.toArray(".project_Item");
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#projects",
+            start: "top 0px",
+            end: "+=1000",
+            pin: true,
+            scrub: 0.2,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
     });
-    $('.project2').on('mouseenter', (event)=>{
-        $('.project2').css({
-            'transform': 'scale(0.8)',
-            'border': '1px solid blue',
-            'transition': '.5s'
-        });
-        $('.project2 img').css({
-            'opacity': '0.7'
-        });
-        $('.project2 p').css({
-            'opacity': '1'
-        });
-    }).on('mouseleave', (event)=>{
-        $('.project2').css({
-            'transform': 'scale(1)',
-            'border': 'none',
-            'opacity': '1',
-            'transition': '.5s'
-        });
-        $('.project2 img').css({
-            'opacity': '1'
-        });
-        $('.project2 p').css({
-            'opacity': '0'
-        });
-    });
+
 });
