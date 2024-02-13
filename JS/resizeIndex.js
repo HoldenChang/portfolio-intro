4773d2d8d2cca4acf349da77cf96fa5b4620d408
Holden Chang <ncsm1752@naver.com>
2024-02-13 09:52:21 -0800

    final fix of resizing bug
    typeo at script src so fixed and worked

---
+++ JS/resizeIndex.js
@@ -0,0 +1,15 @@
+function setIndexHeight() {
+
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    const vwToPixels = (7 * viewportWidth) / 100;
+    const windowHeight = window.innerHeight - vwToPixels;
+    const startDiv = document.querySelector('#start');
+
+    startDiv.style.height = `${windowHeight}px`;
+
+}
+
+setIndexHeight();
+window.addEventListener('resize', setIndexHeight);
+
+
